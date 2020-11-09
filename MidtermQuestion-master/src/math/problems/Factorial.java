0e6dd79ffaddc9aec5c7edd3ecf00ca0766cda74
ipipduh <philippakramou@gmail.com>
2020-11-09 18:19:17 -0500

    third commit

--- MidtermQuestion-master/src/math/problems/Factorial.java
+++ MidtermQuestion-master/src/math/problems/Factorial.java
@@ -1,5 +1,8 @@
 package math.problems;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
+
 public class Factorial {
 
     public static void main(String[] args) {
@@ -9,5 +12,35 @@ public class Factorial {
          *
          */
 
+        // Scanner object for capturing user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number");
+        // Stored the value in variable
+        int num = scan.nextInt();
+        // Called user defined function
+        int factorial = fact(num);
+        System.out.println("Recursion Factorial of " + num + "IS: "+factorial);
+
+        // Find the factorial of this number
+        int number = 5;
+        long factorial1 = 1;
+        int i = 1;
+        while (i <= number) {
+            factorial1 = factorial1 * i;
+            i++;
+        }
+        System.out.println("Iteration Factorial of " + number + " IS: "+factorial);
+        scan.close();
+    }
+
+    static int fact(int n) {
+        int output;
+        if (n==1) {
+            return 1;
+        }
+
+        // Recursion: Function CALLING ITSELF
+        output = fact(n-1) * n;
+        return output;
     }
 }
